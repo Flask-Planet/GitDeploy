1954f08b2942905cba2aaf50438d21fa2c58e194
David Carmichael <carmichaelits@gmail.com>
2023-02-18 23:49:36 +0000

    Removed files that have been replaced or are no longer needed

--- app/api/routes/get_repo_contents.py
+++
@@ -1,8 +0,0 @@
-from app.extensions import security, gitdeploy
-from .. import bp
-
-
-@bp.get('/get-repo-contents')
-@security.login_required('www.login', 'logged_in')
-def get_repo_contents():
-    return {"repo_contents": gitdeploy.get_repo_contents()}
--- wsgi.py
+++
@@ -1,7 +0,0 @@
-from app import create_app
-
-if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True, host="0.0.0.0", port=5500)
-else:
-    wsgi = create_app()
