b489b927c281bf38b1288a742a88a676228f11f5
David Carmichael <carmichaelits@gmail.com>
2023-02-20 21:00:43 +0000

    Fixed FIRST_RUN checker

--- app/www/routes/index.py
+++ app/www/routes/index.py
@@ -7,7 +7,7 @@ from .. import bp
 @bp.get('/')
 def index():
     gitdeploy.read_conf()
-    if not gitdeploy.conf.get("FIRST_RUN"):
+    if gitdeploy.conf.get("FIRST_RUN"):
         return redirect(url_for("www.first_run"))
 
     if session.get("logged_in"):
