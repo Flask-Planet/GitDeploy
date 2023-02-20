1d2068aa908e37359eda74de44af718374492c12
David Carmichael <carmichaelits@gmail.com>
2023-02-20 22:40:39 +0000

    Added webhook statuses to status checker

--- app/api/routes/status.py
+++ app/api/routes/status.py
@@ -5,10 +5,13 @@ from .. import bp
 @bp.get('/status')
 @security.login_required('www.login', 'logged_in')
 def status():
+    gitdeploy.read_conf()
     response = {
         "repo_contents": gitdeploy.get_repo_contents(),
         "satellite_status": gitdeploy.status_satellite(),
         "venv_exists": gitdeploy.env.repo_venv_bin.exists(),
         "packages": gitdeploy.check_installed_packages(),
+        "wh_enabled": gitdeploy.conf.get("WH_ENABLED"),
+        "wh_secret": gitdeploy.conf.get("WH_SECRET"),
     }
     return response
