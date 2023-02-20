bbe6a86e5a61164cf07400acaee710820a4657cd
David Carmichael <carmichaelits@gmail.com>
2023-02-20 21:05:40 +0000

    Added some sleeper loops, and an indication of where the app is running from. (this needs a live indication)

--- gitdeployflask/__main__.py
+++ gitdeployflask/__main__.py
@@ -1,21 +1,32 @@
-from environment import Environment
-from the_nightman import Supervisord, Gunicorn
+from time import sleep
+
+try:
+    from environment import Environment
+    from the_nightman import Supervisord, Gunicorn, supervisor_sock
+except ImportError:
+    from .environment import Environment
+    from .the_nightman import Supervisord, Gunicorn, supervisor_sock
 
 
 class Launcher:
     def __init__(self):
+        self.env = Environment()
         self.supervisord = Supervisord()
         self.gunicorn = Gunicorn()
-        self.env = Environment()
 
     def __enter__(self):
         self.supervisord.start()
+        while True:
+            sleep(1)
+            if supervisor_sock.exists():
+                break
         self.gunicorn.start()
-        return "Running... (Press Ctrl+C to stop)"
+        return "Running on: 0.0.0.0:9898 (Press Ctrl+C to stop)"
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         pass
 
 
-with Launcher() as output:
-    print(output)
+if __name__ == '__main__':
+    with Launcher() as output:
+        print(output)
