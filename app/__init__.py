76ddf47f2a6125cb13f59bb1106f02941b97be46
David Carmichael <carmichaelits@gmail.com>
2023-02-20 21:07:17 +0000

    Added sleeper loop to make sure supervisorctl is running

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,21 @@
+import os
+from time import sleep
+
 from flask import Flask
 
 from app.extensions import bigapp, gitdeploy
 
 gitdeploy.init_supervisorctl()
+
+while True:
+    sleep(1)
+    if gitdeploy.status_supervisorctl():
+        gitdeploy.update_supervisorctl()
+        break
+
+# generate a random secret key
+if not os.environ.get("SECRET_KEY"):
+    os.environ["SECRET_KEY"] = os.urandom(24).hex()
 
 
 def create_app():
