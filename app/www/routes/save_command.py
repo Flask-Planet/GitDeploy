b1c9fe3afec8979cfdd6b278b21a9c33f2495f63
David Carmichael <carmichaelits@gmail.com>
2023-02-20 20:59:19 +0000

    Removed print statements

--- app/www/routes/save_command.py
+++ app/www/routes/save_command.py
@@ -7,9 +7,6 @@ from .. import bp
 @bp.post('/save/command')
 @security.login_required('www.login', 'logged_in')
 def save_command():
-    for key, value in request.form.items():
-        print(f'{key}: {value}')
-
     gitdeploy.read_conf()
     gitdeploy.set_conf('COMMAND', request.form.get("command", None), write=True)
     gitdeploy.write_satellite_ini()
